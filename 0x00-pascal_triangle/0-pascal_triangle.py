0b99b1c59957136deeca0cfe2dbc172e819e3d16
Ukeme Edet <ukemeedet2207@gmail.com>
2024-09-30 19:28:52 +0100

    Refactor pascal_triangle function to generate Pascal's triangle efficiently

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,11 +1,8 @@
 def pascal_triangle(n: int) -> list[list[int]]:
     if n < 1:
         return []
-    triangle = [[1], [1, 1]]
+    triangle = [[1] * (i + 1) for i in range(n)]
     for i in range(2, n):
-        row = [0] * (i + 1)
         for j in range(1, i):
-            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        row[0] = row[-1] = 1
-        triangle.append(row)
-    return triangle[:n]
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+    return triangle
