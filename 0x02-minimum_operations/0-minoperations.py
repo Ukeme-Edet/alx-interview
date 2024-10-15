c46e18b4004725f56165e715e5f2f556f3dc5f16
Ukeme Edet <ukemeedet2207@gmail.com>
2024-10-15 22:22:39 +0100

    Refactor get_prime_factors function to optimize prime factor calculation using a more efficient algorithm

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -14,19 +14,14 @@ def get_prime_factors(n):
     Returns:
         list: list of prime factors
     """
-    fac = [True] * (n + 1)
-    fac[0] = fac[1] = False
-    for i in range(2, int(n**0.5) + 1):
-        if fac[i]:
-            for j in range(i * i, n + 1, i):
-                fac[j] = False
-    facs = [i for i in range(2, n + 1) if fac[i] and not n % i]
     factors = []
-    for i in facs:
-        while not n % i:
-            factors.append(i)
-            n //= i
-    return factors if factors else [n]
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            factors.append(divisor)
+            n //= divisor
+        divisor += 1
+    return factors
 
 
 def minOperations(n):
