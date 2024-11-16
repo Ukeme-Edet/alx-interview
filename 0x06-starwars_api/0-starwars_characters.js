194d5703ad6ac5e417ec9c965a5fbb2c1a25ca2d
Ukeme Edet <ukemeedet2207@gmail.com>
2024-11-16 20:13:55 +0100

    Refactor character fetching to use Promises for improved error handling and readability

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,19 +2,22 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/';
 const movieId = process.argv[2];
-request(url + 'films/' + movieId, (error, _, body) => {
+request(url + 'films/' + movieId, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (error, _, body) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    return;
   }
+  const characters = JSON.parse(body).characters;
+  Promise.all(
+    characters.map(
+      (url) =>
+        new Promise((resolve, reject) =>
+          request(url, (error, response, body) =>
+            error ? reject(error) : resolve(JSON.parse(body).name)
+          )
+        )
+    )
+  )
+    .then((names) => names.forEach((name) => console.log(name)))
+    .catch((error) => console.log(error));
 });
