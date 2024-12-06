3f3382f7e81c2743fc35b071fe278607187adac1
Ukeme Edet <ukemeedet2207@gmail.com>
2024-12-06 19:45:36 +0100

    Refactor island_perimeter function to improve perimeter calculation logic

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,8 +17,10 @@ def island_perimeter(grid):
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
-            if grid[i][j] == 0:
-                for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
-                    if 0 <= i + dx < len(grid) and 0 <= j + dy < len(grid[i]):
-                        perimeter += grid[i + dx][j + dy]
+            if grid[i][j] == 1:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
     return perimeter
